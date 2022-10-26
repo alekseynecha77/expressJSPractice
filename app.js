c2ee982d41f90c49034aeaf3cefef612a2091680
alekseynecha77 <68791158+alekseynecha77@users.noreply.github.com>
2022-10-25 21:55:52 -0400

    update

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+
+const colors = [
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'purple'
+  ];
+  
+app.set('view engine', 'pug');
+
+app.get('/', (req, res)=>{
+    res.render('index');
+});
+
+app.get('/cards', (req, res)=>{
+    res.render('card', {prompt: "Who is buried in Grant's tomb"});
+
+});
+
+app.listen(3000, ()=>{
+console.log("The app is running on localhost:3000");
+
+});
