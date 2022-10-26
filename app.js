a063658ccbb1d52f6f2d4291523dc1524f1e382c
alekseynecha77 <68791158+alekseynecha77@users.noreply.github.com>
2022-10-26 16:30:36 -0400

    adding cookies

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 
 const app = express();
 
@@ -14,7 +15,7 @@ const colors = [
   ];
   
 app.use(bodyParser.urlencoded({extended: false}))
-
+app.use(cookieParser());
 app.set('view engine', 'pug');
 
 app.get('/', (req, res)=>{
@@ -26,10 +27,11 @@ app.get('/cards', (req, res)=>{
 
 });
 app.get('/hello', (req, res)=>{
-    res.render('hello');
+    res.render('hello', {name: req.cookies.username});
 
 });
 app.post('/hello', (req, res)=>{
+    res.cookie('username', req.body.username);
     res.render('hello', {name: req.body.username});
 
 });
