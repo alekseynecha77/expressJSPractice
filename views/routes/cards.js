76aafbd2f6757c23ba3e6195d7a8d33950c1da5a
alekseynecha77 <68791158+alekseynecha77@users.noreply.github.com>
2022-10-28 17:23:50 -0400

    update

--- views/routes/cards.js
+++ views/routes/cards.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const { data } = require('/Users/alekseinecha77/Desktop/expressSimpleApp/expressJSPractice/data/flashCardData.json');
 const { cards } = data;
 
-const colors = [
-    'green',
-    'red',
-    'yello'
-];
+// const colors = [
+//     'green',
+//     'red',
+//     'yello'
+// ];
 
 router.get('/:id', (req, res) => {
     const { side } = req.query;
