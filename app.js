1d8afc28db2b1b4c1e52807a244cb899b4caff7c
alekseynecha77 <68791158+alekseynecha77@users.noreply.github.com>
2022-10-28 16:23:44 -0400

    error handlers and routes

--- app.js
+++ app.js
@@ -4,67 +4,30 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 
-
-const colors = [
-    'red',
-    'orange',
-    'yellow',
-    'green',
-    'blue',
-    'purple'
-  ];
-  
 app.use(bodyParser.urlencoded({extended: false}))
 
 app.use(cookieParser())
 
-app.use((req, res, next)=>{
-    console.log('one');
-    next();
-});
-
-app.use((req, res, next)=>{
-    console.log('two');
-    next();
-});
 
 app.set('view engine', 'pug');
 
-app.get('/', (req, res)=>{
-    const name =  req.cookies.username;
-    if(name){
-    res.render('index', { name });
-    }else{
-        res.redirect('/hello');
-    }
+const routes = require('/Users/alekseinecha77/Desktop/expressSimpleApp/expressJSPractice/views/routes');
+
+app.use(routes);
+
+
+app.use((req, res, next)=>{
+    const err = new Error('Not found');
+    err.status = 404;
+    next(err);
 });
 
-app.get('/cards', (req, res)=>{
-    res.render('card', {prompt: "Who is buried in Grant's tomb", colors});
-
-});
-app.get('/hello', (req, res)=>{
-    const name = req.cookies.username;
-
-    if(name){
-    res.redirect('/');
-    }else{
-        res.render('hello');
-    }
-});
-app.post('/hello', (req, res)=>{
-    res.cookie('username', req.body.username);
-    res.redirect('/');
-
+app.use((err, req, res, next)=>{
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error');
 });
 
-app.post('/goodbye', (req, res)=>{
-    res.clearCookie('username');
-    res.redirect('/hello');
-
-});
-
-app.listen(3000, ()=>{
-console.log("The app is running on localhost:3000");
-
+app.listen(3000, () => {
+    console.log('The application is running on localhost:3000')
 });
