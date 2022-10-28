923ddf5d9fc7a67301b3462fa3130b0c682ea788
alekseynecha77 <68791158+alekseynecha77@users.noreply.github.com>
2022-10-28 19:58:44 -0400

    deletion

--- views/routes/cards.js
+++ views/routes/cards.js
@@ -3,19 +3,13 @@ const router = express.Router();
 const { data } = require('../../data/flashCardData.json');
 const { cards } = data;
 
-const colors = [
-    'green',
-    'red',
-    'yello'
-];
-
 router.get('/:id', (req, res) => {
     const { side } = req.query;
     const{ id } = req.params;
     const text = cards[id][side];
     const{ hint } = cards[id];
 
-    const templateData = { text, hint, colors };
+    const templateData = { text, hint };
     res.render('card', templateData);
 });
 
