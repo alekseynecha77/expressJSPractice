606a45428721550072c314b2434a4a848e79dc44
alekseynecha77 <68791158+alekseynecha77@users.noreply.github.com>
2022-10-28 20:33:12 -0400

    update

--- views/routes/cards.js
+++ views/routes/cards.js
@@ -3,13 +3,34 @@ const router = express.Router();
 const { data } = require('../../data/flashCardData.json');
 const { cards } = data;
 
+
+router.get('/', (req, res) => {
+    const numberOfCards = cards.length;
+    const flashCardId = Math.floor( Math.random() * numberOfCards );
+    res.redirect(`/cards/${flashCardId}`)
+});
+
 router.get('/:id', (req, res) => {
     const { side } = req.query;
     const{ id } = req.params;
+    if( !side ){
+        res.redirect(`/cards/${id}?side=question`)
+    }
+
+    const name = req.cookies.username;
     const text = cards[id][side];
     const{ hint } = cards[id];
 
-    const templateData = { text, hint };
+    const templateData = { id, text, name };
+
+    if(side === 'question'){
+        templateData.hint = hint;
+        templateData.sideToShow = 'answer';
+        templateData.sideToShowDisplay = 'Answer';
+    }else if (side === 'answer'){
+        templateData.sideToShow = 'question';
+        templateData.sideToShowDisplay = 'Question';
+    }
     res.render('card', templateData);
 });
 
